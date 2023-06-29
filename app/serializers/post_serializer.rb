3a5673144a4a7e3c6061514e9f20682a9a6f0326
Baruch Marambi Mwenda <marambib2019@gmail.com>
2023-06-29 15:41:11 +0300

    done

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -2,6 +2,7 @@ class PostSerializer < ActiveModel::Serializer
   attributes :id, :title, :content
 
   belongs_to :author
+  has_many :tags through :post_tags
 
   def short_content
     object.content.length > 40 ? object.content[0..39] + '...' : object.content
